b79cc26e0c4bee0338c33dd75c42f8a31e38f140
gagansachan12 <gagan8sachan@gmail.com>
2026-08-28 22:42:59 +0530

    Add sumOfNaturalNumbers method in NaturalSum class

---
+++ loops/class_problems/sumOfNaturalNumbers.java
@@ -0,0 +1,11 @@
+public class NaturalSum {
+    public static void sumOfNaturalNumbers(int n) {
+        int sum = 0;
+        int counter = 1;
+        while (counter <= n) {
+            sum += counter;
+            counter++;
+        }
+        System.out.println("Sum of numbers from 1 to " + n + " = " + sum);
+    }
+}
