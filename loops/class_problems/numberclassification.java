09a1207730c260e1cfbcc64a3f94070b8c5be3da
gagansachan12 <gagan8sachan@gmail.com>
2026-08-28 22:40:12 +0530

    Add NumberClassifier class to classify numbers

---
+++ loops/class_problems/numberclassification.java
@@ -0,0 +1,11 @@
+public class NumberClassifier {
+    public static void classifyNumber(int number) {
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
