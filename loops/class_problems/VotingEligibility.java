8ed894c83490f9c0884dd04c144f88c1ff80b97e
gagansachan12 <gagan8sachan@gmail.com>
2026-08-28 22:38:36 +0530

    Add VotingChecker class to check voting eligibility

---
+++ loops/class_problems/VotingEligibility.java
@@ -0,0 +1,9 @@
+public class VotingChecker {
+    public static void checkVotingEligibility(int age) {
+        if (age >= 18) {
+            System.out.println("Eligible to vote");
+        } else {
+            System.out.println("Not eligible to vote");
+        }
+    }
+}
